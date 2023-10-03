0415da722482888ee30c6cd3a4b21ae314125072
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-10-03 02:29:25 -0300

    Fix: move the files to right folders.  Changes to be committed: 	renamed:    ft_printf.h -> include/ft_printf.h 	renamed:    ft_printf.c -> src/ft_printf.c

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,11 +6,11 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 02:17:33 by dande-je          #+#    #+#             */
-/*   Updated: 2023/10/02 04:52:23 by dande-je         ###   ########.org.br   */
+/*   Updated: 2023/10/02 22:23:46 by dande-je         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../include/ft_printf.h"
 
 static void	ft_get_flags(const char *format, va_list ap, t_line *line);
 
