39c234979c1879dbe3d40d7565a55f8ce9d6eea5
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-10-15 05:57:47 -0300

    Fix: refactory the code of parse combination.  Changes to be committed: 	modified:   bonus/utils/combination_utils/ft_parse_combination.c

--- bonus/utils/combination_utils/ft_parse_combination.c
+++ bonus/utils/combination_utils/ft_parse_combination.c
@@ -6,34 +6,31 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 19:25:55 by dande-je          #+#    #+#             */
-/*   Updated: 2023/10/14 05:46:46 by dande-je         ###   ########.org.br   */
+/*   Updated: 2023/10/15 04:33:51 by dande-je         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/ft_printf_bonus.h"
 
-size_t	ft_parse_combination(const char *format, t_line *line)
+size_t	ft_parse_combination(const char *format, t_line *line, int i_spec)
 {
-	const char	*next_chr = format;
-
-	if (*next_chr == 'c' || *next_chr == 's' || *next_chr == 'p'\
-		|| *next_chr == 'd' || *next_chr == 'i' || *next_chr == 'u'\
-		|| *next_chr == 'x' || *next_chr == 'X' || *next_chr == '%')
-		return (OFF);
-	while (*next_chr)
+	while (SPEC[i_spec])
+		if (*format == SPEC[i_spec++])
+			return (OFF);
+	while (*format)
 	{
-		if (*next_chr == '#')
+		if (*format == '#')
 		{
 			line->hash = ON;
 			return (JUMP);
 		}
-		else if (*next_chr == ' ')
-			return (ft_parse_space(next_chr, line));
-		else if (*next_chr == '+')
-			return (ft_parse_plus(next_chr, line));
-		else if (*next_chr == '-')
-			return (ft_parse_minus(next_chr, line));
-		next_chr++;
+		else if (*format == ' ')
+			return (ft_parse_space(format, line));
+		else if (*format == '+')
+			return (ft_parse_plus(format, line));
+		else if (*format == '-')
+			return (ft_parse_minus(format, line));
+		format++;
 	}
 	return (OFF);
 }
