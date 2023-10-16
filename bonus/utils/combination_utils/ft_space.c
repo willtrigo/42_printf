19ed087422601871e0490aa0b87b006134ba6b61
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-10-15 22:38:40 -0300

    Feat: Add parse_width in parse space.  Changes to be committed: 	modified:   bonus/utils/combination_utils/ft_space.c

--- bonus/utils/combination_utils/ft_space.c
+++ bonus/utils/combination_utils/ft_space.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 05:12:34 by dande-je          #+#    #+#             */
-/*   Updated: 2023/10/14 05:13:27 by dande-je         ###   ########.org.br   */
+/*   Updated: 2023/10/15 14:17:44 by dande-je         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ ssize_t	ft_parse_space(const char *format, t_line *line)
 	i = -1;
 	while (format[++i])
 	{
+		line->space = ON;
 		if (format[i] == '%' || format[i] == 'd'
 			|| format[i] == 'i' || format[i] == 's')
-		{
-			line->space = ON;
 			return (i);
-		}
+		if (format[i] >= '1' && format[i] <= '9')
+			return (i + ft_parse_width((format + i), line));
 	}
 	return (OFF);
 }
