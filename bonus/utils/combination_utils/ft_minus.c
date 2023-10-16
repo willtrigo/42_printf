0ac2db8d502e05128e7e2c35bbb901ef56fbf421
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-10-15 22:40:28 -0300

    Feat: Add function ft_parse_width in parse minus.  Changes to be committed: 	modified:   bonus/utils/combination_utils/ft_minus.c

--- bonus/utils/combination_utils/ft_minus.c
+++ bonus/utils/combination_utils/ft_minus.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 05:27:37 by dande-je          #+#    #+#             */
-/*   Updated: 2023/10/14 05:45:46 by dande-je         ###   ########.org.br   */
+/*   Updated: 2023/10/15 16:06:34 by dande-je         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@ ssize_t	ft_parse_minus(const char *format, t_line *line)
 	i = -1;
 	while (format[++i])
 	{
+		line->minus = ON;
 		if (format[i] == 'c' || format[i] == 's' || format[i] == 'p'
 			|| format[i] == 'd' || format[i] == 'i' || format[i] == 'u'
 			|| format[i] == 'x' || format[i] == 'X')
-		{
-			line->minus = ON;
 			return (i);
+		if (format[i] >= '1' && format[i] <= '9')
+		{
+			line->minus = ft_parse_width((format + i), line);
+			return (i + ft_parse_width((format + i), line));
 		}
 	}
 	return (OFF);
