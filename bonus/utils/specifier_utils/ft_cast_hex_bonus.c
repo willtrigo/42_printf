3e43baecfaddd0a73b2c5201b66d2fa3e98429fd
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-10-16 18:21:42 -0300

    Feat: Add flag minus and witdh in the %p %x %X.  Changes to be committed: 	modified:   bonus/utils/specifier_utils/ft_cast_hex_bonus.c 	modified:   include/ft_printf_bonus.h

--- bonus/utils/specifier_utils/ft_cast_hex_bonus.c
+++ bonus/utils/specifier_utils/ft_cast_hex_bonus.c
@@ -6,18 +6,20 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 23:38:25 by dande-je          #+#    #+#             */
-/*   Updated: 2023/10/16 11:10:10 by dande-je         ###   ########.org.br   */
+/*   Updated: 2023/10/16 16:34:32 by dande-je         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/ft_printf_bonus.h"
 
 static void	ft_get_hex(t_line *line, t_uli hex, int len, int spec);
+static void	ft_flag_add_hex(t_line *line, int hex_len);
 
 size_t	ft_cast_hex_ptr(va_list ap, t_line *line, int spec)
 {
-	const t_uli	hex = va_arg(ap, t_uli);
+	t_uli	hex;
 
+	hex = va_arg(ap, t_uli);
 	if (spec == CHK_HEX_PTR)
 	{
 		if (!hex)
@@ -28,16 +30,25 @@ size_t	ft_cast_hex_ptr(va_list ap, t_line *line, int spec)
 			ft_get_hex(line, hex, 0, spec);
 		}
 	}
-	if (line->width >= ON)
-		while (line->width-- - ft_get_width_int_len((t_ulli)hex) > OFF)
+	if (line->width >= ON && line->minus == OFF)
+		while (line->width-- - 1 - ft_get_width_int_len((t_ulli)hex) > OFF)
+			ft_chr_add(&line->str, ft_chr_new(' '), line);
+	if (line->minus >= ON && ft_get_width_int_len((t_ulli)hex) == 10)
+		while ((line->width-- - ft_get_width_int_len((t_ulli)hex)) > OFF)
 			ft_chr_add(&line->str, ft_chr_new(' '), line);
 	return (JUMP);
 }
 
 size_t	ft_cast_hex_lw_up(va_list ap, t_line *line, int spec)
 {
+	int				hex_len;
 	const t_ulli	hex = (t_ulli)va_arg(ap, t_ui);
 
+	hex_len = 1;
+	if (hex >= 16)
+		hex_len = ft_get_width_int_len(hex);
+	if (hex_len == 10)
+		hex_len = 8;
 	if (!hex)
 		ft_str_add("0", line);
 	else
@@ -51,9 +62,7 @@ size_t	ft_cast_hex_lw_up(va_list ap, t_line *line, int spec)
 		else
 			ft_get_hex(line, hex, 0, CHK_HEX_UP);
 	}
-	if (line->width >= ON)
-		while ((line->width-- - 10) > OFF)
-			ft_chr_add(&line->str, ft_chr_new(' '), line);
+	ft_flag_add_hex(line, hex_len);
 	return (JUMP);
 }
 
@@ -71,4 +80,14 @@ static void	ft_get_hex(t_line *line, t_uli hex, int len, int spec)
 			ft_get_hex(line, hex / 16, ++len, CHK_HEX_LW);
 		ft_chr_add(&line->str, ft_chr_new(HEX_LW[hex % 16]), line);
 	}
+}
+
+static void	ft_flag_add_hex(t_line *line, int hex_len)
+{
+	if (line->width >= ON && line->minus == OFF)
+		while (line->width-- - 1 - hex_len > OFF)
+			ft_chr_add(&line->str, ft_chr_new(' '), line);
+	if (line->minus >= ON)
+		while ((line->width-- - hex_len) > OFF)
+			ft_chr_add(&line->str, ft_chr_new(' '), line);
 }
