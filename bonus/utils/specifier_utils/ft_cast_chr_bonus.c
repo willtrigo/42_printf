61318a7869aa49452e0b06454aa71ea63d2b4921
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-10-15 22:26:55 -0300

    Feat: add flags width and minus in cast chr.  Changes to be committed: 	modified:   bonus/utils/specifier_utils/ft_cast_chr_bonus.c

--- bonus/utils/specifier_utils/ft_cast_chr_bonus.c
+++ bonus/utils/specifier_utils/ft_cast_chr_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 21:33:53 by dande-je          #+#    #+#             */
-/*   Updated: 2023/10/15 05:32:57 by dande-je         ###   ########.org.br   */
+/*   Updated: 2023/10/15 19:59:01 by dande-je         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@ size_t	ft_cast_chr(va_list ap, t_line *line, int spec)
 	const int	chr = va_arg(ap, int);
 
 	(void)spec;
+	if (line->width >= ON && line->minus == OFF)
+		while ((line->width-- - 1) > OFF)
+			ft_chr_add(&line->str, ft_chr_new(' '), line);
 	ft_chr_add(&line->str, ft_chr_new(chr), line);
+	if (line->width >= ON && line->minus >= ON)
+		while ((line->width-- - 1) > OFF)
+			ft_chr_add(&line->str, ft_chr_new(' '), line);
 	return (0);
 }
